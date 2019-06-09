f0a420568e7e6d7c20748ebb92e8c7e756557825
Andrea Mazzoni <andreamazzoni78@gmail.com>
2019-06-10 00:40:25 +0200

    Print admin user creation

--- jupyterhub/entrypoint.sh
+++ jupyterhub/entrypoint.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 useradd -m $ADMIN_USERNAME
 echo $ADMIN_USERNAME:$ADMIN_PASSWORD | chpasswd
+echo User $ADMIN_USERNAME created.
 
 if [ "$1" = 'jupyterhub' ]; then
     exec jupyterhub "$@"
